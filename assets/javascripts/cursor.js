42e898c8ff8eab047e60bcf3ddc2eaa3904eeb40
Arnaud Levy <contact@arnaudlevy.com>
2021-02-19 14:33:42 +0100

    point

---
+++ assets/javascripts/cursor.js
@@ -0,0 +1,38 @@
+const div = document.createElement('div');
+div.className = 'cursor'
+const cursor = document.querySelector('.cursor');
+
+let mouseX = 0;
+let mouseY = 0;
+
+let cursorX = 0;
+let cursorY = 0;
+
+let size = 32;
+
+let speed = 0.2;
+
+function animate() {
+    let distX = mouseX - cursorX;
+    let distY = mouseY - cursorY;
+
+    cursorX = cursorX + (distX * speed);
+    cursorY = cursorY + (distY * speed);
+
+    size = size + 1;
+
+    cursor.style.left = cursorX + 'px';
+    cursor.style.top = cursorY + 'px';
+    cursor.style.width = size + 'px';
+    cursor.style.height = size + 'px';
+
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+document.addEventListener('mousemove', (event) => {
+    mouseX = event.pageX;
+    mouseY = event.pageY;
+    size = 32;
+})
